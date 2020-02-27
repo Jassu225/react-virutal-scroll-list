7cfbee8e6b8eecd12ade088c4e48062d3fa4ece8
Jassu <jaswanthsaisattenapalli@gmail.com>
2020-02-27 23:37:07 +0530

    add prod build

---
+++ build/precache-manifest.1027c3d7393fb5c180a0c6c4b1d6105b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9735227f1c13444f7eaeccee3739b920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe9cdae93e3953600ec",
+    "url": "/static/css/main.ba092a25.chunk.css"
+  },
+  {
+    "revision": "4d99764204bf77d91d81",
+    "url": "/static/js/2.ec93a0b6.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.ec93a0b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe9cdae93e3953600ec",
+    "url": "/static/js/main.86a18b64.chunk.js"
+  },
+  {
+    "revision": "1d01cd660b207d541a73",
+    "url": "/static/js/runtime-main.59bb450b.js"
+  },
+  {
+    "revision": "1d7626e6cbbdc02be15b16fa36add9ae",
+    "url": "/static/media/loader.1d7626e6.gif"
+  }
+]);
