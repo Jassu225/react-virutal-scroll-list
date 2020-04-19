4a3c48f3009f2761efa8fbf37a0ae1627dedc036
Jassu <jaswanthsaisattenapalli@gmail.com>
2020-04-19 11:59:14 +0530

    follow open graph protocol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css';
 import DataTable from './components/DataTable/DataTable';
 import { useDispatch, useSelector } from 'react-redux';
 import { getList } from './store/getters';
 import { fetchList } from './store/actions';
+import ogImage from './assets/images/og-image.png';
+
+const appData = {
+  title: 'react virtual scroll list',
+  description: 'A virtual scroll list created using only functional components & hooks in React.js',
+};
 
 function App(props) {
   const dispatch = useDispatch();
@@ -37,6 +44,14 @@ function App(props) {
 
   return (
     <div className="App">
+      <Helmet>
+          <title>{appData.title}</title>
+          <meta property="og:url" content={window.location.origin} />
+          <meta property="og:title" content={appData.title} />
+          <meta property="og:description" content={appData.description} />
+          <meta name="description" content={appData.description} />
+          <meta property="og:image" content={ogImage} />
+      </Helmet>
       <DataTable
         columns={columns}
         rows={list}
